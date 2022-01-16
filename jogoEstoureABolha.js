d26ba7ef46c8872fd1eacc5c78880ce18ee5331d
Lara Simmer <larafsimmer@gmail.com>
2022-01-16 20:50:15 -0300

    Atualização no jogo Estoure a bolha

--- jogoEstoureABolha.js
+++ jogoEstoureABolha.js
@@ -1,3 +1,23 @@
+function criaBolha() {
+    let x = Math.floor(Math.random()*800);
+    let y = Math.floor(Math.random()*500);
+
+    let bolha = $("<div class='bolha'></div>")
+
+    bolha.css("left", x+ "px");
+    bolha.css("top", y+ "px");
+
+    $(document.body).append(bolha);
+}
+
 $(document).ready(function() {
-   
+   $("#comecar").on("click", function() {
+       setInterval(criaBolha, 1000);
+   });
+
+   $(".bolha").on("click", function() {
+       $(this).fadeOut();
+       let placar = $("#placar").html();
+       placar = placar + 1;
+   })
 });
