032283c06a7ceb1e4c62da28843d76055143f8f5
Lara Simmer <larafsimmer@gmail.com>
2022-01-17 20:41:08 -0300

    Finalizando jogo Estoure a bolha com contagem de placar

--- jogoEstoureABolha.js
+++ jogoEstoureABolha.js
@@ -1,23 +1,29 @@
+let placar = 0;
+
 function criaBolha() {
     let x = Math.floor(Math.random()*800);
     let y = Math.floor(Math.random()*500);
 
-    let bolha = $("<div class='bolha'></div>")
+    let bolha = $("<div class='bolha'></div>");
 
     bolha.css("left", x+ "px");
     bolha.css("top", y+ "px");
 
+    bolha.on("click",function() {
+        $(this).fadeOut("fast");
+        placar++;
+        atualizaPlacar();
+    })
+
     $(document.body).append(bolha);
+}
+
+function atualizaPlacar() {
+    $("#placar").html(placar);
 }
 
 $(document).ready(function() {
    $("#comecar").on("click", function() {
        setInterval(criaBolha, 1000);
    });
-
-   $(".bolha").on("click", function() {
-       $(this).fadeOut();
-       let placar = $("#placar").html();
-       placar = placar + 1;
-   })
 });
